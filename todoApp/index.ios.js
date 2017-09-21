eb0b1126793cf6506764adde1f6e043328657839
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-09-21 16:49:46 +0500

    update

--- todoApp/index.ios.js
+++ todoApp/index.ios.js
@@ -1,53 +1,122 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  TouchableHighlight,
+  TextInput,
+  ListView
+} from "react-native";
+
+const styles = require("./app/styles");
+
+import * as firebase from "firebase";
+//var firebase = require("firebase");
+// Initialize Firebase
+const firebseConfig = {
+  apiKey: "AIzaSyDf5VmiD8SpVG5AUXcGIawKaRPPJoSNb2A",
+  authDomain: "react-native-todo-7511e.firebaseapp.com",
+  databaseURL: "https://react-native-todo-7511e.firebaseio.com",
+  projectId: "react-native-todo-7511e",
+  storageBucket: ""
+  //messagingSenderId: "605643278992"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class todoApp extends Component {
+  constructor(props) {
+    super(props);
+    var myFirebaseRef = firebseConfig.databaseURL;
+    this.itemsRef = myFirebaseRef.child("items");
+
+    this.state = {
+      newTodo: "",
+      todoSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
+    };
+
+    this.items = [];
+  }
+
+  componentDidMount() {
+    // When a todo is added
+    this.itemsRef.on("child_added", dataSnapshot => {
+      this.items.push({ id: dataSnapshot.key(), text: dataSnapshot.val() });
+      this.setState({
+        todoSource: this.state.todoSource.cloneWithRows(this.items)
+      });
+    });
+
+    // When a todo is removed
+    this.itemsRef.on("child_removed", dataSnapshot => {
+      this.items = this.items.filter(x => x.id !== dataSnapshot.key());
+      this.setState({
+        todoSource: this.state.todoSource.cloneWithRows(this.items)
+      });
+    });
+  }
+
+  addTodo() {
+    if (this.state.newTodo !== "") {
+      this.itemsRef.push({
+        todo: this.state.newTodo
+      });
+      this.setState({
+        newTodo: ""
+      });
+    }
+  }
+
+  removeTodo(rowData) {
+    this.itemsRef.child(rowData.id).remove();
+  }
+
+  renderRow(rowData) {
+    return (
+      <TouchableHighlight
+        underlayColor="#dddddd"
+        onPress={() => this.removeTodo(rowData)}
+      >
+        <View>
+          <View style={styles.row}>
+            <Text style={styles.todoText}>{rowData.text.todo}</Text>
+          </View>
+          <View style={styles.separator} />
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={styles.appContainer}>
+        <View style={styles.titleView}>
+          <Text style={styles.titleText}>My Todos</Text>
+        </View>
+        <View style={styles.inputcontainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => this.setState({ newTodo: text })}
+            value={this.state.newTodo}
+          />
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => this.addTodo()}
+            underlayColor="#dddddd"
+          >
+            <Text style={styles.btnText}>Add!</Text>
+          </TouchableHighlight>
+        </View>
+        <ListView
+          dataSource={this.state.todoSource}
+          renderRow={this.renderRow.bind(this)}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('todoApp', () => todoApp);
+AppRegistry.registerComponent("todoApp", () => todoApp);
